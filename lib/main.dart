bc162e5c9f761e7ad224c7108dfa262a0665e1ea
MJI-Abir <mjiabir12007@gmail.com>
2023-08-01 02:22:43 +0600

    created the BMI Calculator app

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:bmi_calculator/screens/input_page.dart';
 
-void main() {
-  runApp(const BMICalculator());
-}
+void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
-  const BMICalculator ({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
       home: InputPage(),
     );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  const InputPage({super.key});
-
-  @override
-  State<InputPage> createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('BMI Calculator'),
-      ),
-      body: const Center(
-        child: Text('Body text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => '',
-        child: const Icon(Icons.add),
-      ),
-    )
-    ;
   }
 }
